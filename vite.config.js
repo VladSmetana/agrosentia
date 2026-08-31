0daf5910610b650598c72f4f9688c34317e46456
VladSmetana <120550721+VladSmetana@users.noreply.github.com>
2026-08-31 20:05:54 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
-  base: '/agrosentia/',
+  base: '/',
   plugins: [injectHTML(), FullReload(['./partials/**/*.html'])],
   build: {
     sourcemap: true,
